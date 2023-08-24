73f3ee521d268ae5b9d645ead392aedab8945562
BravinR <bravinrutoh@gmail.com>
2023-08-24 15:10:26 -0400

    Added the New 1

--- yamlLoader.java
+++ yamlLoader.java
@@ -1,37 +1,37 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 @ConfigurationProperties(prefix = "spring")
-public class YamlLoader{
+public class YamlConfigLoader{
 
+    @Value("${yml.folder.path}")
     private String ymlFolderPath;
 
-    public void setYmlFolderPath(String ymlFolderPath) {
-        this.ymlFolderPath = ymlFolderPath;
-    }
+    public Map<String, AlertConfig> readYamlFiles() {
+        Map<String, AlertConfig> resultMap = new HashMap<>();
+        File folder = new File(ymlFolderPath);
 
-    public Map<String, Data> readYamlFiles() {
-        Map<String, Data> resultMap = new HashMap<>();
-        Resource[] yamlResources = new PathMatchingResourcePatternResolver().getResources(ymlFolderPath + "*.yml");
-        Yaml yaml = new Yaml();
-
-        for (Resource resource : yamlResources) {
-            try (InputStream inputStream = resource.getInputStream()) {
-                Data data = yaml.loadAs(inputStream, Data.class);
-                resultMap.put(data.getKey(), data);
-            } catch (IOException e) {
-                e.printStackTrace();
+        if (folder.exists() && folder.isDirectory()) {
+            File[] yamlFiles = folder.listFiles((dir, name) -> name.endsWith(".yml"));
+            if (yamlFiles != null) {
+                Yaml yaml = new Yaml();
+                for (File yamlFile : yamlFiles) {
+                    try (FileInputStream fis = new FileInputStream(yamlFile)) {
+                        AlertConfig alertConfig = yaml.loadAs(fis, AlertConfig.class);
+                        resultMap.put(data.getKey(), data);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
         return resultMap;
@@ -47,15 +47,15 @@ import java.util.Map;
 
 @SpringBootTest
 @Slf4j
-public class YamlReaderServiceTest {
+public class YamlConfigLoaderTest{
 
     @Autowired
-    private YamlReaderService yamlReaderService;
+    private YamlConfigLoader yamlConfigLoader;
 
     @Test
     public void testReadYamlFiles() {
-        Map<String, Data> resultMap = yamlReaderService.readYamlFiles();
+        Map<String, AlertConfig> resultMap = yamlReaderService.readYamlFiles();
         log.info("Loaded YAML data: {}", resultMap);
-        // Add assertions here to validate the loaded data
+        // TODO:Add assertions here to validate the loaded data
     }
 }
